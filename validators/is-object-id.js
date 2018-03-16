56aa22e33cf7b1b00ea8657025542ded6d9560fe
Adam Reis <adam@reis.nz>
2018-03-16 18:51:44 +1300

    [enhance] Allow empty flag for isObjectId check

--- validators/is-object-id.js
+++ validators/is-object-id.js
@@ -9,17 +9,25 @@ const {ObjectId} = mongoose.Types;
 /**
  * Object ID validator
  */
-module.exports = function isObjectId(value) {
+module.exports = function isObjectId(value, allowEmpty = false) {
 
   //Handle arrays
   if (Array.isArray(value)) {
+    if (allowEmpty) {
+      value = value.filter(id => !!id);
+    }
     if (!value.every(id => ObjectId.isValid(id))) {
       throw new Error(`Expected an array of ObjectId's`);
     }
   }
 
   //Single ID
-  else if (!ObjectId.isValid(value)) {
-    throw new Error('Expected an ObjectId');
+  else {
+    if (allowEmpty && !value) {
+      return;
+    }
+    if (!ObjectId.isValid(value)) {
+      throw new Error('Expected an ObjectId');
+    }
   }
 };
